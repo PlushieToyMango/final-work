bce2eb3f7f3d63fc64a2957d7dfd031a6ab227af
PlushieToyMango <marklekhner2000@gmail.com>
2024-04-01 12:52:29 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество строк в массиве:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] originalArray = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Введите строку {i + 1}:");
+            originalArray[i] = Console.ReadLine();
+        }
+        int count = 0;
+        foreach (string str in originalArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+        string[] newArray = new string[count];
+        int index = 0;
+        foreach (string str in originalArray)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[index++] = str;
+            }
+        }
+        Console.WriteLine("\nНовый массив со строками длиной меньше или равной 3:");
+        foreach (string str in newArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
